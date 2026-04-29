4f258bb8df190b8e76594eaf6bb7ff90f524fd6b
Ashvin Desai <ashvindesai@live.com>
2026-04-29 13:55:06 +0530

    Temp: debug wrapper to capture load error

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
-module.exports = require('../server/dist/app').default;
+let app;
+try {
+  app = require('../server/dist/app').default;
+} catch (err) {
+  app = (_req, res) => {
+    res.status(500).json({ loadError: err.message, stack: err.stack });
+  };
+}
+
+module.exports = app;
